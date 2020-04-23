5221f0b75b3302fea914e16730715cff3020f7c8
Michele Gualco <mgualco.contractor@libertyglobal.com>
2020-04-23 09:52:27 +0200

    bf_pytest: renamed boardfarm_pytest to bf_pytest
    Signed-off-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Change-Id: Ie194aa4c692be3e96a4ba300fe9ca112210969f2
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm-pytest/+/4872
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='boardfarm_pytest',
+    name='bf_pytest',
     version='1.0.0',
     description='Makes boardfarm available as a pytest plugin',
     author='Various',
@@ -12,6 +12,6 @@ setup(
     package_data={'': ['*.txt', '*.json', '*.cfg', '*.md', '*.csv']},
     include_package_data=True,
     # the following makes a plugin available to pytest
-    entry_points={"pytest11": ["boardfarm = boardfarm_pytest"]},
+    entry_points={"pytest11": ["boardfarm = bf_pytest"]},
     # custom PyPI classifier for pytest plugins
     classifiers=["Framework :: Pytest"])
