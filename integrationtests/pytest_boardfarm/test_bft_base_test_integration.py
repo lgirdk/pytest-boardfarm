ea657511612de87a7338943367e14aa0557eb57d
Michele Gualco <mgualco.contractor@libertyglobal.com>
2020-10-11 16:53:47 +0200

    integrationtests: test_bft_base_test_integration
    checks that the bft_base_testsi derived tests integration with pytest works as
    it should.
    
    Depends-on: Ib38b68135f572edc49a92730cc03d61f6b7254b8
    Signed-off-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Change-Id: Iab9d0bee123d18ddb0ef64601fd768b38082ced3
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/pytest-boardfarm/+/8675
    Reviewed-by: Filippo Carra <fcarra@libertyglobal.com>
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

---
+++ integrationtests/pytest_boardfarm/test_bft_base_test_integration.py
@@ -0,0 +1,64 @@
+import pytest
+from boardfarm.bft import logger
+from boardfarm.exceptions import CodeError, ContingencyCheckError, SkipTest
+from boardfarm.tests import bft_base_test
+
+
+class myBftBaseTest(bft_base_test.BftBaseTest):
+    """None of the tests in this module should make the overall pytest fail."""
+
+    @staticmethod
+    def ok(*args, **kwargs):
+        pass
+
+    def __init__(self, *args, **kwargs):
+        self.result_grade = ""
+        self.td_step = type("tstclass", (object,), {"td_final_result": None})()
+        self.config = type("tstclass1", (object,), {"devices": [], "retry": 0})()
+        board = type(
+            "tstclass2", (object,), {"consoles": [], "touch": myBftBaseTest.ok}
+        )()
+        self.dev = type("tstclass3", (object,), {"board": None})()
+        self.dev.board = board
+        self.logged = {}
+
+    def recover(self):
+        pass
+
+
+class AlwaysPasses(myBftBaseTest):
+    def test_main(self):
+        logger.info("This always passes")
+
+
+@pytest.mark.xfail(strict=True, reason="Intentional CodeError", raises=CodeError)
+class AlwaysFails(myBftBaseTest):
+    def test_main(self):
+        logger.info("This test always fails, but pytest overall does NOT!")
+        """We have to use CodeError instead the more appropriate TestError as
+        the latter is intercepted by pytest (due to the Test prefix) which
+        generates a warning."""
+        raise CodeError()
+
+
+class ThisIsSkippedFromBFT(myBftBaseTest):
+    def test_main(self):
+        logger.info("This is skipped from bft_base_test pytest overall must NOT fail!")
+        logger.info("It raises a SkipTest but with 'SKIP' in self.result_grade")
+        raise SkipTest()
+
+
+@pytest.mark.skip(reason="This is skipped  with a marker")
+class ThisWouldFailButIsSkipped(myBftBaseTest):
+    def test_main(self):
+        assert 0, "Should never get to this point!!!!!"
+
+
+@pytest.mark.xfail(
+    strict=True, raises=SkipTest, reason="Intentional Contingency check error"
+)
+class ThisIsSkippedFromPytest(myBftBaseTest):
+    def test_main(self):
+        logger.info("This is a Contigency Check failure")
+        logger.info("it raises a SkipTest but with 'CC FAIL' in self.result_grade")
+        raise ContingencyCheckError("Forced failure for intergration test")
