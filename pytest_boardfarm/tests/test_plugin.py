28b66d6ca22048e6447f7f69175e4046cc29f7ba
Musthafa Kariyaparambil <mkariyaparambil.contractor@libertyglobal.com>
2021-08-03 12:57:45 +0530

    pytest_boardfarm: plugin: Sanitise pytest xml output
    Signed-off-by: Musthafa Kariyaparambil <mkariyaparambil.contractor@libertyglobal.com>
    Change-Id: I7be11cc59c2cd2a6763d85f9115f0f9e1848750d
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/pytest-boardfarm/+/12905
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

---
+++ pytest_boardfarm/tests/test_plugin.py
@@ -0,0 +1,45 @@
+from unittest.mock import MagicMock
+
+from pytest_boardfarm.plugin import pytest_unconfigure
+
+
+class _PathStub:
+    def __init__(self, name, content=None):
+        self.name = name
+        self.parent = ""
+        self.content = content
+        self.written_content = None
+
+    def exists(true):
+        return True
+
+    def read_text(self):
+        return self.content
+
+    def rename(self, path):
+        self.renamed_path = path
+
+    def write_text(self, content):
+        self.written_content = content
+
+
+def test_pytest_unconfigure_remove_control_chars(mocker):
+    xml_content = r"sample text"
+    mocked_path = _PathStub("xmlpath", xml_content)
+    config = MagicMock(autospec=True)
+    config.option.xmlpath = "xmlpath"
+    mocker.patch("pytest_boardfarm.plugin.Path", return_value=mocked_path)
+    pytest_unconfigure(config)
+    assert "" not in mocked_path.written_content
+    assert "sample text" == mocked_path.written_content
+
+
+def test_pytest_unconfigure_remove_escape_sequence(mocker):
+    xml_content = "\x1b[00m\x1b[01;31mfile.zip\x1b[00m\r\n\x1b[01;31m"
+    mocked_path = _PathStub("xmlpath", xml_content)
+    config = MagicMock(autospec=True)
+    config.option.xmlpath = "xmlpath"
+    mocker.patch("pytest_boardfarm.plugin.Path", return_value=mocked_path)
+    pytest_unconfigure(config)
+    expected_content = "file.zip"
+    assert expected_content == mocked_path.written_content
