c2632ff8ea3b6cbcfa829aec007e47da19b69889
Oleksii Popov <opopov.contractor@libertyglobal.com>
2022-05-24 13:19:26 +0200

    feat: add browser data fixture to integrate PoM in tests
    Signed-off-by: Oleksii Popov <opopov.contractor@libertyglobal.com>
    Change-Id: Ie07bdaf250fa6545c0822ba014e5793362213b45
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/pytest-boardfarm/+/15752
    Reviewed-by: Musthafa Kariyaparambil <mkariyaparambil.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Tested-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- pytest_boardfarm/boardfarm_fixtures.py
+++ pytest_boardfarm/boardfarm_fixtures.py
@@ -1,13 +1,21 @@
 """pytest boardfarm fixtures."""
 
+import logging
+from typing import Generator
+
 from _pytest.config import Config
 from boardfarm.lib.boardfarm_config import BoardfarmConfig
 from boardfarm.lib.device_manager import DeviceManager
+from boardfarm.templates.lan import LAN
+from boardfarm_docsis.templates.cable_modem import CableModem
+from boardfarm_lgi_shared.lib.gui.gui_helper import get_web_driver
 from pytest import fixture
 
 from pytest_boardfarm.boardfarm_plugin import BOARDFARM_PLUGIN_NAME, BoardfarmPlugin
 from pytest_boardfarm.exceptions import BoardfarmPluginError
 from pytest_boardfarm.lib.test_logger import TestLogger
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class ContextStorage:  # pylint: disable=too-few-public-methods
@@ -63,3 +71,33 @@ def device_manager(pytestconfig: Config) -> DeviceManager:
     :return: boardfarm device manager
     """
     return get_boardfarm_plugin(pytestconfig).device_manager
+
+
+@fixture(scope="function")
+def browser_data(
+    device_manager: DeviceManager, boardfarm_config: BoardfarmConfig
+) -> Generator:
+    # pylint: disable=redefined-outer-name
+    """Provide selenium driver data to tests.
+
+    :param device_manager: device manager fixture
+    :param boardfarm_config: boardfarm_config fixture
+    :return: initialized webdriver instance, gateway IP and GUI password
+    """
+    lan = device_manager.get_device_by_type(LAN)
+    board = device_manager.get_device_by_type(CableModem)
+    driver = get_web_driver(lan)
+    # Set board SKU and board hardware type as driver properties
+    # to be able to customize POM
+    driver.sku = boardfarm_config.get_board_sku()
+    driver.board_model = boardfarm_config.get_board_model()
+    _LOGGER.info("Initialized %s %s webdriver.", driver.sku, driver.board_model)
+
+    gateway_ipv4 = (
+        board.lan_private_gateway
+        if boardfarm_config.get_prov_mode() == "disabled"
+        else board.lan_gateway
+    )
+
+    yield driver, gateway_ipv4, board.gui_password
+    driver.quit()
