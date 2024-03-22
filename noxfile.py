d6b08026f732c6ba89e5009e196ad3efb085056d
Michele Gualco <msalvatoregualco@libertyglobal.com>
2024-03-22 07:04:06 +0100

    Revert "ci: nox: use uv as backend"
    This reverts commit 326cf009ea1c0ea787c63b5530fe531f789179e9.
    
    Change-Id: I13557beafde90a4490d994ecbbd4f5f7f6bd3fbc
    Signed-off-by: Michele Gualco <msalvatoregualco@libertyglobal.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/pytest-boardfarm/+/5884
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- noxfile.py
+++ noxfile.py
@@ -1,25 +1,19 @@
 """Lint and test pytest-boardfarm on multiple python environments."""
-import os
 
 import nox
-
-os.environ[
-    "UV_INDEX_URL"
-] = "http://pypi.boardfarm.upclabs.com:3141/root/boardfarm-lgi/+simple/"
 
 _PYTHON_VERSIONS = ["3.11"]
 
 # Fail nox session when run a program which
 # is not installed in its virtualenv
 nox.options.error_on_external_run = True
-nox.options.default_venv_backend = "uv"
 
 
 @nox.session(python=_PYTHON_VERSIONS)
 def lint(session: nox.Session) -> None:
     """Lint pytest-boardfarm."""
     session.install("--upgrade", "--pre", "boardfarm3")
-    session.install("--upgrade", "-e", ".[dev]")
+    session.install("--upgrade", ".[dev]")
     session.run("ruff", "format", "--check", ".")
     session.run("ruff", "check", ".")
     session.run("mypy", "pytest_boardfarm3")
@@ -30,7 +24,7 @@ def lint(session: nox.Session) -> None:
 def pylint(session: nox.Session) -> None:
     """Lint pytest-boardfarm using pylint without dev dependencies."""
     session.install("--upgrade", "--pre", "boardfarm3")
-    session.install("--upgrade", "pylint")
+    session.install("--upgrade", ".", "pylint")
     session.run("pylint", "pytest_boardfarm3")
 
 
@@ -38,5 +32,5 @@ def pylint(session: nox.Session) -> None:
 def test(session: nox.Session) -> None:
     """Test pytest-boardfarm."""
     session.install("--upgrade", "--pre", "boardfarm3")
-    session.install("--upgrade", "-e", ".[test]")
+    session.install("--upgrade", ".[test]")
     session.run("pytest", "unittests")
